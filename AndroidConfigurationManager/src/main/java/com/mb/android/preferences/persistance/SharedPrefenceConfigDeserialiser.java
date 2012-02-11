c4e5623cbc8f4386eded807d4c5459b3c6b78afa
Miles Burton <miles@Yosemite.(none)>
2012-02-11 12:11:13 +0000

    @Miles catching classCastException and logging if we cannot deserialise a value

--- AndroidConfigurationManager/src/main/java/com/mb/android/preferences/persistance/SharedPrefenceConfigDeserialiser.java
+++ AndroidConfigurationManager/src/main/java/com/mb/android/preferences/persistance/SharedPrefenceConfigDeserialiser.java
@@ -1,68 +1,77 @@
 package com.mb.android.preferences.persistance;
 
+import java.lang.reflect.Field;
+
 import android.content.SharedPreferences;
+import android.util.Log;
+
 import com.mb.android.preferences.annotations.ConfigMetadata;
 import com.mb.android.preferences.domain.Config;
 import com.mb.android.preferences.reflection.ConfigMetadataReflector;
 import com.mb.android.preferences.reflection.ConfigMetadataReflector.ConfigValueAction;
 
-import java.lang.reflect.Field;
-
 public class SharedPrefenceConfigDeserialiser implements ConfigDeserialiser {
 
-    private ConfigMetadataReflector reflector = new ConfigMetadataReflector();
-    private SharedPreferences preferences;
-    private KeyGenerator keyGenerator = new KeyGenerator();
+	private final String TAG = SharedPrefenceConfigDeserialiser.class.getCanonicalName();
 
-    public SharedPrefenceConfigDeserialiser(SharedPreferences preferences) {
-        this.preferences = preferences;
-    }
+	private ConfigMetadataReflector reflector = new ConfigMetadataReflector();
+	private SharedPreferences preferences;
+	private KeyGenerator keyGenerator = new KeyGenerator();
 
-    public Config deserialise(final Config config, final String id) {
-        config.setId(id);
+	public SharedPrefenceConfigDeserialiser(SharedPreferences preferences) {
+		this.preferences = preferences;
+	}
 
-        ConfigValueAction putEditorAction = new ConfigValueAction() {
+	public Config deserialise(final Config config, final String id) {
+		config.setId(id);
 
+		ConfigValueAction putEditorAction = new ConfigValueAction() {
 
-            public void process(Field field, ConfigMetadata fieldMetadata) {
-                Object value = getPreferenceValueForField(id, fieldMetadata);
-                if (value != null)
-                    setObjectValueForField(config, field, value);
-            }
+			public void process(Field field, ConfigMetadata fieldMetadata) {
+				Object value = getPreferenceValueForField(id, fieldMetadata);
+				if (value != null)
+					setObjectValueForField(config, field, value);
+			}
 
-        };
+		};
 
-        reflector.processMetadata(config, putEditorAction);
+		reflector.processMetadata(config, putEditorAction);
 
-        return config;
-    }
+		return config;
+	}
 
-    public String getType(String id) {
-        return preferences.getString(keyGenerator.createTypeKey(id), "");
-    }
+	public String getType(String id) {
+		return preferences.getString(keyGenerator.createTypeKey(id), "");
+	}
 
-    private void setObjectValueForField(Config config, Field field, Object value) {
-        try {
-            field.set(config, value);
-        } catch (IllegalArgumentException e) {
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        }
-    }
+	private void setObjectValueForField(Config config, Field field, Object value) {
+		try {
+			field.set(config, value);
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		}
+	}
 
-    private Object getPreferenceValueForField(String configId, ConfigMetadata fieldMetadata) {
-        Object value = null;
-        switch (fieldMetadata.type()) {
-            case String:
-            case StringList:
-                value = preferences.getString(keyGenerator.createKey(configId, fieldMetadata.id()), "");
-                break;
-            case Boolean:
-                value = preferences.getBoolean(keyGenerator.createKey(configId, fieldMetadata.id()), false);
-                break;
-        }
-        return value;
-    }
+	private Object getPreferenceValueForField(String configId, ConfigMetadata fieldMetadata) {
 
+		String key = keyGenerator.createKey(configId, fieldMetadata.id());
+		Object value = null;
+
+		try {
+			switch (fieldMetadata.type()) {
+			case String:
+			case StringList:
+				value = preferences.getString(key, "");
+				break;
+			case Boolean:
+				value = preferences.getBoolean(key, false);
+				break;
+			}
+		} catch (ClassCastException ex) {
+			Log.e(TAG, "Could not deserialise '" + key + "'");
+		}
+		return value;
+	}
 }
--- AndroidConfigurationManager/src/main/java/com/mb/android/preferences/ui/UIPreferenceBuilder.java
+++ AndroidConfigurationManager/src/main/java/com/mb/android/preferences/ui/UIPreferenceBuilder.java
@@ -1,7 +1,12 @@
 package com.mb.android.preferences.ui;
 
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+
 import android.preference.ListPreference;
 import android.preference.Preference;
+
 import com.mb.android.preferences.annotations.ConfigDescription;
 import com.mb.android.preferences.annotations.ConfigMetadata;
 import com.mb.android.preferences.annotations.ConfigOption;
@@ -11,87 +16,82 @@ import com.mb.android.preferences.persistance.KeyGenerator;
 import com.mb.android.preferences.reflection.ConfigMetadataReflector;
 import com.mb.android.preferences.reflection.ConfigMetadataReflector.ConfigValueAction;
 
-import java.lang.reflect.Field;
-import java.util.ArrayList;
-import java.util.List;
-
 public class UIPreferenceBuilder {
-    private KeyGenerator keyGenerator = new KeyGenerator();
-    private PreferenceFactory preferenceFactory = null;
+	private KeyGenerator keyGenerator = new KeyGenerator();
+	private PreferenceFactory preferenceFactory = null;
 
-    public List<Preference> getPreferencesFor(final Config config) {
-        final List<Preference> preferences = new ArrayList<Preference>();
+	public List<Preference> getPreferencesFor(final Config config) {
+		final List<Preference> preferences = new ArrayList<Preference>();
 
-        ConfigMetadataReflector configMetadataReflector = new ConfigMetadataReflector();
+		ConfigMetadataReflector configMetadataReflector = new ConfigMetadataReflector();
 
-        ConfigValueAction putEditorAction = new ConfigValueAction() {
+		ConfigValueAction putEditorAction = new ConfigValueAction() {
 
+			public void process(Field field, ConfigMetadata configMetadata) {
+				ConfigDescription configDescription = getConfigDescription(field);
+				Preference preference = getPreferenceFor(config, configMetadata, configDescription);
+				populateDefaults(field, preference);
+				preferences.add(preference);
+			}
+		};
 
-            public void process(Field field, ConfigMetadata configMetadata) {
-                ConfigDescription configDescription = getConfigDescription(field);
-                Preference preference = getPreferenceFor(config, configMetadata, configDescription);
-                populateDefaults(field, preference);
-                preferences.add(preference);
-            }
-        };
+		configMetadataReflector.processMetadata(config, putEditorAction);
+		return preferences;
+	}
 
-        configMetadataReflector.processMetadata(config, putEditorAction);
-        return preferences;
-    }
+	private ConfigDescription getConfigDescription(Field field) {
+		ConfigDescription fieldDescription = (ConfigDescription) field.getAnnotation(ConfigDescription.class);
 
-    private ConfigDescription getConfigDescription(Field field) {
-        ConfigDescription fieldDescription = (ConfigDescription) field.getAnnotation(ConfigDescription.class);
+		if (fieldDescription == null)
+			throw new MissingAnnotationException();
+		return fieldDescription;
+	}
 
-        if (fieldDescription == null)
-            throw new MissingAnnotationException();
-        return fieldDescription;
-    }
+	private Preference getPreferenceFor(Config config, ConfigMetadata configMetadata, ConfigDescription fieldDescription) {
+		String key = keyGenerator.createKey(config.getId(), configMetadata.id()); // this is wrong! shouldn't know about a key generator
 
-    private Preference getPreferenceFor(Config config, ConfigMetadata configMetadata, ConfigDescription fieldDescription) {
-        String key = keyGenerator.createKey(config.getId(), configMetadata.id()); // this is wrong! shouldn't know about a key generator
+		Preference preference = preferenceFactory.createPreference(configMetadata);
 
-        Preference preference = preferenceFactory.createPreference(configMetadata);
+		preference.setKey(key);
+		preference.setTitle(fieldDescription.title());
+		if (!"".equals(fieldDescription.description()))
+			preference.setSummary(fieldDescription.description());
 
-        preference.setKey(key);
-        preference.setTitle(fieldDescription.title());
-        if (!"".equals(fieldDescription.description()))
-            preference.setSummary(fieldDescription.description());
+		return preference;
+	}
 
-        return preference;
-    }
+	private void populateDefaults(Field field, Preference preference) {
+		if (preference instanceof ListPreference)
+			populateDefaults(field, (ListPreference) preference);
+	}
 
-    private void populateDefaults(Field field, Preference preference) {
-        if (preference instanceof ListPreference)
-            populateDefaults(field, (ListPreference) preference);
-    }
+	private void populateDefaults(Field field, ListPreference preference) {
+		ConfigOptions configOptions = (ConfigOptions) field.getAnnotation(ConfigOptions.class);
+		if (configOptions == null)
+			return;
 
-    private void populateDefaults(Field field, ListPreference preference) {
-        ConfigOptions configOptions = (ConfigOptions) field.getAnnotation(ConfigOptions.class);
-        if (configOptions == null)
-            return;
+		String[] titleArray = new String[configOptions.values().length];
+		String[] valueArray = new String[configOptions.values().length];
 
-        String[] titleArray = new String[configOptions.values().length];
-        String[] valueArray = new String[configOptions.values().length];
+		for (int i = 0; i < configOptions.values().length; i++) {
+			ConfigOption configOption = configOptions.values()[i];
+			titleArray[i] = configOption.title();
+			valueArray[i] = configOption.value();
+		}
 
-        for (int i = 0; i < configOptions.values().length; i++) {
-            ConfigOption configOption = configOptions.values()[i];
-            titleArray[i] = configOption.title();
-            valueArray[i] = configOption.value();
-        }
+		preference.setEntries(titleArray);
+		preference.setEntryValues(valueArray);
+	}
 
-        preference.setEntries(titleArray);
-        preference.setEntryValues(valueArray);
-    }
+	public void setPreferenceFactory(PreferenceFactory preferenceFactory) {
+		this.preferenceFactory = preferenceFactory;
+	}
 
-    public void setPreferenceFactory(PreferenceFactory preferenceFactory) {
-        this.preferenceFactory = preferenceFactory;
-    }
+	public interface PreferenceFactory {
+		Preference createPreference(ConfigMetadata metadata);
+	}
 
-    public interface PreferenceFactory {
-        Preference createPreference(ConfigMetadata metadata);
-    }
-
-    public static class MissingAnnotationException extends RuntimeException {
-        private static final long serialVersionUID = 1922639213002486492L;
-    }
+	public static class MissingAnnotationException extends RuntimeException {
+		private static final long serialVersionUID = 1922639213002486492L;
+	}
 }
