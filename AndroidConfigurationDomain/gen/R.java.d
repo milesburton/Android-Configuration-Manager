e12ff5c374ff8bd040b71d4fbabddec1b8136d32
Miles Burton <miles@mnetcs.com>
2012-07-01 13:13:15 +0100

    @Miles removed unrequired files

--- AndroidConfigurationDomain/gen/R.java.d
+++
@@ -1,3 +0,0 @@
-/home/miles/workspace/AirCollection/Android-Configuration-Manager/AndroidConfigurationDomain/gen/com/mb/android/R.java \
- : /home/miles/workspace/AirCollection/Android-Configuration-Manager/AndroidConfigurationDomain/res/drawable/ic_launcher.png \
-/home/miles/workspace/AirCollection/Android-Configuration-Manager/AndroidConfigurationDomain/AndroidManifest.xml \
